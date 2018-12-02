3694af35b3f67e4a591c016fbb6a87d559d007aa
chencong-plan <1042738887@qq.com>
2018-12-02 13:08:40 +0800

    update config

---
+++ order/order-server/src/main/java/cc/ccoder/sell/order/controller/EnvController.java
@@ -0,0 +1,26 @@
+package cc.ccoder.sell.order.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+/**
+ * @author 聪聪 www.ccoder.cc
+ * @date 2018/12/2 12:49
+ */
+@RestController
+@RequestMapping("/env")
+public class EnvController {
+
+    @Value("${env}")
+    private String env;
+
+    @GetMapping("/print")
+    public String test(){
+        return env;
+    }
+
+}
