d1529fe231e8a1f69485df77fbc7634d39417f0f
chencong-plan <1042738887@qq.com>
2018-12-02 14:23:59 +0800

    添加refreshScope启动刷新配置

--- order/order-server/src/main/java/cc/ccoder/sell/order/controller/EnvController.java
+++ order/order-server/src/main/java/cc/ccoder/sell/order/controller/EnvController.java
@@ -1,6 +1,7 @@
 package cc.ccoder.sell.order.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import javax.validation.Valid;
  */
 @RestController
 @RequestMapping("/env")
+@RefreshScope
 public class EnvController {
 
     @Value("${env}")
